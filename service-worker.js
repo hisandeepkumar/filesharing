45e0c46266cbb29af8d32f156f5c00c727d8a3ce
Sandeep  Yadav <84236760+hisandeepkumar@users.noreply.github.com>
2025-03-27 22:37:17 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "infinity-cache-v1";
+const urlsToCache = [
+  "./index.html",
+  "./styles.css",
+  "./script.js",
+  "./icons/icon-192x192.png",
+  "./icons/icon-512x512.png",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
